07629e4f77552ed55f21768dbce1aaaf1a687a78
Prakash5209 <prakashkchaudhary5209@gmail.com>
2024-04-28 22:16:38 +0545

    dumb chat in progress

---
+++ chat/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+from account.models import User
+
+
+#this is temoporary chat
+class ChatModel(models.Model):
+    me_user = models.ForeignKey(User,on_delete=models.CASCADE)
+    other_user = models.ForeignKey(User,on_delete=models.CASCADE
+    text = models.TextField()
+
+    def __str__(self):
+        return f"{self.me_user}-{slef.other_user}"
